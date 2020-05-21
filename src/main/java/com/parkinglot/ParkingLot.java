c59fc755f351bcc28426a31ada93a7ac0be9d912
PalakPartani <partanipalak16@gmail.com>
2020-05-21 05:35:07 -0700

    ADDED:feature to unpark vehicle

--- src/main/java/com/parkinglot/ParkingLot.java
+++ src/main/java/com/parkinglot/ParkingLot.java
@@ -1,7 +1,18 @@
 package com.parkinglot;
 
 public class ParkingLot {
+    Object vehicle;
+
     public boolean parkVehicle(Object vehicle) {
+        this.vehicle = vehicle;
         return true;
+    }
+
+    public boolean unParkVehicle(Object vehicle) {
+        if (this.vehicle == vehicle) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/parkinglot/ParkingLotTest.java
+++ src/test/java/com/parkinglot/ParkingLotTest.java
@@ -7,15 +7,24 @@ import org.junit.Test;
 public class ParkingLotTest {
 
     ParkingLot parkingLot;
+    Object vehicle;
 
     @Before
     public void setUp() throws Exception {
         parkingLot = new ParkingLot();
+        vehicle = new Object();
     }
 
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
-        boolean isParked = parkingLot.parkVehicle(new Object());
+        boolean isParked = parkingLot.parkVehicle(vehicle);
         Assert.assertTrue(isParked);
+    }
+
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        parkingLot.parkVehicle(vehicle);
+        boolean isUnParked = parkingLot.unParkVehicle(vehicle);
+        Assert.assertTrue(isUnParked);
     }
 }
